8a62477776c4e3a17ed219fb27d04acea69d7d5d
Anders Flemmen <andersflemmen@gmail.com>
2018-12-11 22:38:46 +0100

    Fixed getInput with prefixing

--- src/main/kotlin/julekalender/Util.kt
+++ src/main/kotlin/julekalender/Util.kt
@@ -4,5 +4,5 @@ import java.io.File
 import java.nio.file.Paths
 
 fun getInput(task: Int): File {
-    return File("${Paths.get("").toAbsolutePath()}${File.separator}input${File.separator}$task")
+    return File("${Paths.get("").toAbsolutePath()}${File.separator}input${File.separator}${"%02d".format(task)}")
 }
